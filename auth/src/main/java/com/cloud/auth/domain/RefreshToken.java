e2218a4f7d65711c0801c713bea6a404150d6206
leonardo <leonardopsantos95@gmail.com>
2026-01-07 00:59:38 -0300

    base project
    Created domain classes

---
+++ auth/src/main/java/com/cloud/auth/domain/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.cloud.auth.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_uuid", referencedColumnName = "uuid")
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    private boolean revoked;
+}
---
+++ auth/src/main/java/com/cloud/auth/domain/User.java
@@ -0,0 +1,33 @@
+package com.cloud.auth.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled = true;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAtt;
+}
