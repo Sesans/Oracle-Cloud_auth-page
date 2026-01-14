bbd8f74520200b6382d5571d7bc7021f8e082ffd
leonardo <leonardopsantos95@gmail.com>
2026-01-14 12:37:40 -0300

    updated serviceTest for better test coverage, add new required Mock and failure cases

--- auth/src/test/java/com/cloud/auth/service/UserServiceTest.java
+++ auth/src/test/java/com/cloud/auth/service/UserServiceTest.java
@@ -3,18 +3,22 @@ package com.cloud.auth.service;
 import com.cloud.auth.domain.*;
 import com.cloud.auth.domain.dto.UserRegisterDTO;
 import com.cloud.auth.domain.dto.UserResponseDTO;
+import com.cloud.auth.exception.BusinessException;
 import com.cloud.auth.repository.UserRepository;
+import com.cloud.auth.security.TokenService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -22,6 +26,10 @@ import static org.mockito.Mockito.*;
 class UserServiceTest {
     @Mock
     UserRepository userRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
+    @Mock
+    TokenService tokenService;
     @InjectMocks
     UserService userService;
 
@@ -44,11 +52,53 @@ class UserServiceTest {
 
     @Test
     void save_shouldCreateUserSuccessfully() {
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
         when(userRepository.save(any(User.class))).thenReturn(user);
 
         UserResponseDTO responseDTO = userService.save(requestDTO);
 
         assertEquals(user.getEmail(), responseDTO.email());
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void save_shouldThrowBusinessException(){
+        when(userRepository.existsByEmail(anyString())).thenReturn(true);
+
+        BusinessException ex = assertThrows(BusinessException.class,
+                () -> userService.save(requestDTO));
+
+        assertEquals("Email already registered!", ex.getMessage());
+    }
+
+    @Test
+    void upsert_shouldInsertTheUserSuccessfully(){
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        User user1 = userService.upsert(user.getFirstName(), user.getEmail(), user.getUserOrigin().toString());
+
+        assertEquals(user1.getFirstName(), user.getFirstName());
+        assertEquals(user1.getEmail(), user.getEmail());
+        assertEquals(user1.getUserOrigin(), user.getUserOrigin());
+
+        verify(userRepository, times(1)).save(any(User.class));
+        verify(userRepository, times(1)).findByEmail(anyString());
+    }
+
+    @Test
+    void upsert_shouldUpdateTheUserSuccessfully(){
+        String newName = "Novo Nome";
+        String email = user.getEmail();
+        when(userRepository.findByEmail(email)).thenReturn(Optional.ofNullable(user));
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        User user1 = userService.upsert(newName, email, "GOOGLE");
+
+        assertNotNull(user1);
+        assertEquals(newName, user1.getFirstName());
+        assertEquals(email, user1.getEmail());
 
         verify(userRepository, times(1)).save(any(User.class));
     }
